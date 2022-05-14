4bc9dd10e7e464a9392dfb497bfd2f1993e4345a
here cacao <ixerr@mail.ru>
2022-05-14 19:14:34 +0500

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `consolewrite`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-emotion"]
+};
+
+export default config;
